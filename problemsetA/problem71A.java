4ac6db1b92254907dfce2d849b81ede021f04a8f
Khadiga Gamal <87450539+KHgamal@users.noreply.github.com>
2022-10-07 18:36:42 +0200

    Add files via upload

---
+++ problemsetA/problem71A.java
@@ -0,0 +1,29 @@
+
+import java.util.Scanner;
+
+public class problem71A{
+public static void main (String[] args){
+
+    Scanner input= new Scanner(System.in);
+
+    int n=input.nextInt();
+    String[] list = new String[n];
+    for(int i=0;i<n;i++){
+        String word=input.next();
+        if(word.length()>10){
+            String newWord=String.valueOf(word.charAt(0))
+                    +(word.length()-2)
+                    + word.charAt(word.length()-1);
+
+            list[i]=newWord;
+        }
+        else{
+            list[i]=word;
+        }
+    }
+    for (String s : list) {
+        System.out.println(s);
+    }
+
+}
+}
